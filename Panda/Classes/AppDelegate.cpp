c6d7edb326ec0ebe3a700a4688f98d0d87fb22e7
Hangil Lee <prios@kaist.ac.kr>
2013-03-15 22:36:47 +0900

    일단 생각나는대로 구조만들어봄
    일단 생각나는대로 구조만들어봄

--- Panda/Classes/AppDelegate.cpp
+++ Panda/Classes/AppDelegate.cpp
@@ -19,25 +19,16 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
-    // initialize director
     CCDirector *pDirector = CCDirector::sharedDirector();
     pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
-
-    // turn on display FPS
     pDirector->setDisplayStats(true);
-
-    // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
-
-    // create a scene. it's an autorelease object
     CCScene *pScene = HelloWorld::scene();
 
-    // run
     pDirector->runWithScene(pScene);
     return true;
 }
 
-// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
     CCDirector::sharedDirector()->stopAnimation();
@@ -45,7 +36,6 @@ void AppDelegate::applicationDidEnterBackground()
     SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
 }
 
-// this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
     CCDirector::sharedDirector()->startAnimation();
--- Panda/Classes/HelloWorldScene.cpp
+++ Panda/Classes/HelloWorldScene.cpp
@@ -7,15 +7,10 @@ CCScene* HelloWorld::scene()
     CCScene * scene = NULL;
     do 
     {
-        // 'scene' is an autorelease object
         scene = CCScene::create();
         CC_BREAK_IF(! scene);
-
-        // 'layer' is an autorelease object
         HelloWorld *layer = HelloWorld::create();
         CC_BREAK_IF(! layer);
-
-        // add layer as a child to scene
         scene->addChild(layer);
     } while (0);
 
@@ -76,11 +71,7 @@ bool HelloWorld::init()
         // 3. Add add a splash screen, show the cocos2d splash image.
         CCSprite* pSprite = CCSprite::create("HelloWorld.png");
         CC_BREAK_IF(! pSprite);
-
-        // Place the sprite on the center of the screen
         pSprite->setPosition(ccp(size.width/2, size.height/2));
-
-        // Add the sprite to HelloWorld layer as a child layer.
         this->addChild(pSprite, 0);
 
         bRet = true;
